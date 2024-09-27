4ef5a7d3cb12c466454a90b6c8499d95ce3a9f7d
HyunJunggggg <123992376+HyunJunggggg@users.noreply.github.com>
2024-09-27 15:24:41 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -1,6 +1,31 @@
+//lesson3 - code
 let horseName = "Awesomeguy";
+console.log(horseName);
+horseName = "Sunny";
 console.log(horseName);
 let horseAge = 5;
 console.log(horseAge);
 let isHorseInside = true;
 console.log(isHorseInside);
+
+//lesson4 - code
+console.log(typeof true)
+const STABLE_MONTHLY_FEE = 165; // Upper case preferred than lower case 
+console.log(STABLE_MONTHLY_FEE);
+
+console.log(STABLE_MONTHLY_FEE*3);
+console.log("The stable monthly fee is " + STABLE_MONTHLY_FEE);
+console.log("My horse's name is "+ horseName + " and he is " + (horseAge - 3) +" years old");
+console.log(`My horse's name is ${horseName} and costs $${STABLE_MONTHLY_FEE} to board him`);
+
+console.log((58+33)/6);
+console.log("Hi"+" there");
+console.log("Hi"-" there");
+console.log("hi" +2 ) // type coercion
+console.log("2"+2); // string
+console.log(2 -"2"); // type coercion
+console.log(true+2); //  true =1 , false = 0  , type coercion 
+
+
+
+
